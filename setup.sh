6ebf3b08af9c3ea722c7946ddf44c1ac89296e7a
Luke Martin Vaughan <luke.martin.vaughan@cern.ch>
2025-10-27 20:43:11 +0100

    working setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#setupATLAS -c el9
+setupATLAS --quiet
 mkdir AthAnalysis && cd AthAnalysis
 mkdir source build run
 cd build
 acmSetup --sourcedir=../source AthAnalysis,25.2.35
+acm new_skeleton MyPackage
 
 # Copy over files
 cp ../../AODtracks/MyPackageAlg.* ../source/MyPackage/src
 cp ../../AODtracks/CMakeLists.txt ../source/MyPackage/
 cp ../../AODtracks/myJobOptions.py ../run/
+
+acm compile
+
+cd ..
