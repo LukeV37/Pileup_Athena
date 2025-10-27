353345798bae73203d83d2c53a278dc762b9ee08
Luke Martin Vaughan <luke.martin.vaughan@cern.ch>
2025-10-27 21:05:45 +0100

    use local data samples

--- AODtracks/myJobOptions.py
+++ AODtracks/myJobOptions.py
@@ -1,4 +1,7 @@
-jps.AthenaCommonFlags.FilesInput = [ "/eos/user/k/khanov/pileup/full_geant_mc21_14TeV/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.merge.AOD.e8481_s4149_r14701_r14702_tid33629038_00/AOD.33629038._000001.pool.root.1" ]
+jps.AthenaCommonFlags.FilesInput = [ "../../data/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.merge.AOD.e8481_s4149_r14701_r14702_tid33629038_00/AOD.33629038._000044.pool.root.1" ]
+#jps.AthenaCommonFlags.FilesInput = [ "../../data/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.merge.AOD.e8481_s4038_r14362_r14367/AOD.32628369._000019.pool.root.1" ]
+#jps.AthenaCommonFlags.FilesInput = [ "../../data/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.merge.AOD.e8481_s4149_r14697_r14702/AOD.33629011._000001.pool.root.1" ]
+
 jps.AthenaCommonFlags.AccessMode = "ClassAccess"
 
 alg = CfgMgr.MyPackageAlg('AnalysisAlg')
